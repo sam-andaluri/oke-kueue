9342019e65a3a5d6a2c32a03796f32c36fe433e3
sam-andaluri <andaluri@gmail.com>
2025-01-04 18:43:03 -0500

    add load test

---
+++ submit-job-loadtest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set error handling
+set -euo pipefail
+
+# Default values
+NUM_REQUESTS=10
+ENDPOINT="http://$LOADBALANCER_IP/submit-job"
+
+# Make parallel requests
+for i in $(seq 1 $NUM_REQUESTS); do
+    curl -X POST "$ENDPOINT" &
+done
+
+wait
+
+echo "Done"
+
